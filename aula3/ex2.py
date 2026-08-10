eecb77cb1e5a3d6b7454530c3dad04583d992369
Ysterllane <131874791+Ysterllane@users.noreply.github.com>
2026-08-10 12:01:27 -0300

    Add files via upload

---
+++ aula3/ex2.py
@@ -0,0 +1,15 @@
+# resto da divisão %
+
+10%3 # = 3
+10%2 # = 0
+
+# Divisão exata //
+
+10//3 # = 3
+
+# igual ==
+# diferente !=
+# > maior que
+# < menor que
+# >= maior ou igual
+# <= menor ou igual
