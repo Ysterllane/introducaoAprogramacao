9a09c6ad9fc0a9ba592cdbcc32baf09970814575
Ysterllane <131874791+Ysterllane@users.noreply.github.com>
2026-08-12 10:46:50 -0300

    Add files via upload

---
+++ aula4/ex2.py
@@ -0,0 +1,10 @@
+num:float = float(input("Digite um número: "))
+
+if num == 0:
+    print("Seu número é zero")
+
+elif num > 0:
+    print("Seu número é", num, "e é positivo.")
+
+else:
+    print("Seu núemero é ", num, "e é negativo.")
