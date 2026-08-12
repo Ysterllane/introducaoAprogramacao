4a4d91723b88599356979c17472638d6c18786bd
Ysterllane <131874791+Ysterllane@users.noreply.github.com>
2026-08-12 10:46:17 -0300

    Create ex1.py

---
+++ aula4/ex1.py
@@ -0,0 +1,7 @@
+idade:int = int(input("Qual sua idade? "))
+
+if idade >= 18:
+    print("Você tem ", idade, "anos e é maior de idade.")
+
+else:
+    print("Você tem ", idade, "anos e é menor de idade.")
