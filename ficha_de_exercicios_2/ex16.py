502db8da531cd204bae0eea87d0b16ddd39a06b1
Yasmin Nunes Rifan de Lima [EWBJ] <20262ewbj0127@belojardim.ifpe>
2026-09-15 20:52:03 -0300

    Inicio da questao 16 da lista 2

---
+++ ficha_de_exercicios_2/ex16.py
@@ -0,0 +1,16 @@
+# 16. Um sistema de login permite no máximo 3 tentativas de senha. Caso o usuário erre todas, o acesso deve ser bloqueado — o programa precisa interromper as tentativas assim que esse limite for atingido.
+
+senha = "abacate"
+
+for i in range(3):
+    tentativa = input("Digite sua senha: ")
+    if senha == tentativa:
+        print("Você acessou o sistema.")
+        break
+    elif i == 2:
+        print("Acesso bloqueado.")
+
+        
+
+
+
